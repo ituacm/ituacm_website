bf6b3be5fec27bc3c977dd4890ebe753a25b9f4d
arcaerdogar <arcaerdogar@gmail.com>
2024-09-04 01:19:33 +0300

    sponsors section style adjustment.

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,13 +1,10 @@
-import React from 'react'
-import '../Sponsors/Sponsors.css'
-import AcmWhite from '../../assets/acm-white.png'
+import React from "react";
+import "../Sponsors/Sponsors.css";
+import AcmWhite from "../../assets/acm-white.png";
 
 function Sponsors() {
-    
   return (
-    <body>
-        
-    <div class="logos">
+    <div class="sponsors-container">
       <div class="logos-slide">
         <img src={AcmWhite} />
         <img src={AcmWhite} />
@@ -20,7 +17,7 @@ function Sponsors() {
       </div>
 
       <div class="logos-slide">
-        <img src={AcmWhite}/>
+        <img src={AcmWhite} />
         <img src={AcmWhite} />
         <img src={AcmWhite} />
         <img src={AcmWhite} />
@@ -30,8 +27,7 @@ function Sponsors() {
         <img src={AcmWhite} />
       </div>
     </div>
-  </body>
-  )
+  );
 }
 
-export default Sponsors
+export default Sponsors;
