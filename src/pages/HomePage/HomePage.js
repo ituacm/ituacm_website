317600a0353a32fb46595222661189844374da81
Yusuf Yiğit Yıldırım <161453604+YusufYigitYildirim@users.noreply.github.com>
2024-09-04 02:01:13 +0300

    resolve conflicts

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,24 +2,16 @@ import React from "react";
 import HomeIntro from "../../components/HomeIntro/HomeIntro";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
-<<<<<<< HEAD
-import Stats from "../../components/Stats/Stats";
-=======
 import Sponsors from "../../components/Sponsors/Sponsors";
->>>>>>> bf6b3be5fec27bc3c977dd4890ebe753a25b9f4d
 
 function HomePage() {
   return (
     <div className="home-page-containers">
       <Navbar />
       <HomeIntro />
-<<<<<<< HEAD
-      <Stats/>
-=======
       <Sponsors/>
       
 
->>>>>>> bf6b3be5fec27bc3c977dd4890ebe753a25b9f4d
       <Footer />
     </div>
   );
