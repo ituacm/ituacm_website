95e876944a3abb8f01d64f227a594cd46e4b92c7
arcaerdogar <arcaerdogar@gmail.com>
2024-09-03 18:02:10 +0300

    postcard added.

--- src/pages/EventsPage/EventsPage.js
+++ src/pages/EventsPage/EventsPage.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import PostCard from "../../components/PostCard/PostCard";
+import { useLoaderData } from "react-router-dom";
 
 function EventsPage() {
+  const [events, setEvents] = useState(useLoaderData());
   return (
     <div className="events-page-container">
       <Navbar />
+      <PostCard
+        image={"https://ari24.com/uploads/2021/11/617f04d38d31c.jpg"}
+        title={"Tanışma Toplantısı"}
+        description={"Relive the excitement of our recent hackathon."}
+        startingDate={new Date()}
+        // endingDate={"14-05-2024"}
+        location={"Merkezi Derslik A13"}
+        time={"13.00 pm"}
+      />
       <Footer />
     </div>
   );
