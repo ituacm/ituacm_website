242ff6cab43590930cc3a538e2161df1b797896f
Yusuf Yiğit Yıldırım <161453604+YusufYigitYildirim@users.noreply.github.com>
2024-09-04 21:30:48 +0300

    entire changes

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,35 +1,29 @@
 import React from 'react'
 import '../Stats/Stats.css'
+import ItuAcmWhite from '../../assets/ituacm-logo-white.png'
 
 function Stats() {
   return (
-    <div className='stats-container'>
-    <div class="stats">
-        <div class="stats-header">
-            <h2>Stats</h2>
+    <div className="stats-container">
+      <div className="stats">
+        <div className="stat">
+          <div className="stat-icon">👥</div>
+          <h2 className="stat-detail">100+ Active Members</h2>
+          <div className="wave-animation"></div>
         </div>
-        <div class="stats-content">
-            <div class="stat-item">
-                <div class="stat-icon">👥</div>
-                <div class="stat-text">
-                    <h3>100+ Active Members</h3>
-                </div>
-            </div>
-            <div class="stat-item">
-                <div class="stat-icon">🎟️</div>
-                <div class="stat-text">
-                    <h3>20+ Sponsors</h3>
-                </div>
-            </div>
-            <div class="stat-item">
-                <div class="stat-icon">🛠️</div>
-                <div class="stat-text">
-                    <h3>4 Technical Squads</h3>
-                </div>
-            </div>
+        <div className="stat">
+          <div className="stat-icon">🤝</div>
+          <h2 className="stat-detail">20+ Sponsors</h2>
+          <div className="wave-animation"></div>
         </div>
+        <div className="stat">
+          <div className="stat-icon">🛠️</div>
+          <h2 className="stat-detail">4 Technical Squads</h2>
+          <div className="wave-animation"></div>
+        </div>
+      </div>
+      <div className='stats-logo'><img src={ItuAcmWhite}></img></div>
     </div>
-</div>
   )
 }
 
