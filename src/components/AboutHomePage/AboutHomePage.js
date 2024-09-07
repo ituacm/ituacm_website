e127848343efd19dd451a2ef68bbe5687f1b5fd8
Boitsy <derin.meric20042004@gmail.com>
2024-09-07 11:21:36 +0300

    Now it works based on how far you scrolled.

--- src/components/AboutHomePage/AboutHomePage.js
+++ src/components/AboutHomePage/AboutHomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './AboutHomePage.css';
 
@@ -8,8 +8,52 @@ const AboutHomePage = () => {
     const handleLearnMore = () => {
         navigate('/about'); 
   };
+
+
+  const [isBetween, setIsBetween] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+        const viewHeight = window.innerHeight;
+
+
+        let startRange;
+        let endRange;
+        if(viewHeight<=630){
+           startRange = viewHeight * 0.6; 
+           endRange = viewHeight * 1.2;
+        } 
+        else if(viewHeight<=750){
+            startRange = viewHeight * 0.9; 
+            endRange = viewHeight * 1.3;
+        }
+        else{
+            startRange = viewHeight * 0.7; 
+            endRange = viewHeight * 1.1;
+        }
+
+        
+        if (scrollPosition >= startRange && scrollPosition <= endRange) {
+        setIsBetween(true); 
+      } else {
+        setIsBetween(false);
+      }
+    };
+        
+        window.addEventListener('scroll', handleScroll);
+
+        
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+    
+      
+      const combinedClasses = `image-card ${isBetween ? 'scrolled' : ''}`;
+  
   return (
-    <div className="hover-image-card">
+    <div className={combinedClasses}>
       <img
         src="https://images.pexels.com/photos/7551762/pexels-photo-7551762.jpeg"
         alt="ReplaceImage"
