9af794a3ab72c79ea2df406211ce78423d20f02d
İsmet Arca Erdoğar <arcaerdogar@gmail.com>
2024-09-07 12:21:04 +0300

    scroll event adjustment.

--- src/components/AboutHomePage/AboutHomePage.js
+++ src/components/AboutHomePage/AboutHomePage.js
@@ -1,57 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './AboutHomePage.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./AboutHomePage.css";
 
 const AboutHomePage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLearnMore = () => {
-        navigate('/about'); 
+  const handleLearnMore = () => {
+    navigate("/about");
   };
-
 
   const [isBetween, setIsBetween] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-        const scrollPosition = window.scrollY;
-        const viewHeight = window.innerHeight;
+      const scrollPosition = window.scrollY;
+      const viewHeight = window.innerHeight;
 
+      let startRange;
+      let endRange;
+      if (viewHeight <= 630) {
+        startRange = viewHeight * 1.5;
+        endRange = viewHeight * 5;
+      } else if (viewHeight <= 750) {
+        startRange = viewHeight * 1.5;
+        endRange = viewHeight * 5;
+      } else {
+        startRange = viewHeight * 1.5;
+        endRange = viewHeight * 5;
+      }
 
-        let startRange;
-        let endRange;
-        if(viewHeight<=630){
-           startRange = viewHeight * 0.6; 
-           endRange = viewHeight * 1.2;
-        } 
-        else if(viewHeight<=750){
-            startRange = viewHeight * 0.9; 
-            endRange = viewHeight * 1.3;
-        }
-        else{
-            startRange = viewHeight * 0.7; 
-            endRange = viewHeight * 1.1;
-        }
-
-        
-        if (scrollPosition >= startRange && scrollPosition <= endRange) {
-        setIsBetween(true); 
+      if (scrollPosition >= startRange && scrollPosition <= endRange) {
+        setIsBetween(true);
       } else {
         setIsBetween(false);
       }
     };
-        
-        window.addEventListener('scroll', handleScroll);
 
-        
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, []);
-    
-      
-      const combinedClasses = `image-card ${isBetween ? 'scrolled' : ''}`;
-  
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const combinedClasses = `image-card ${isBetween ? "scrolled" : ""}`;
+
   return (
     <div className={combinedClasses}>
       <img
@@ -61,12 +54,18 @@ const AboutHomePage = () => {
       />
       <div className="overlay">
         <h2 className="header">Who are we?</h2>
-        <p className="paragraph">Lorem ipsum odor amet, consectetuer adipiscing elit. Enim morbi nunc turpis hendrerit accumsan, 
-            ultricies magna finibus. Eleifend molestie efficitur vulputate porttitor natoque quisque adipiscing. 
-            Erat condimentum aenean tempor sagittis sem pharetra pellentesque. Etiam proin metus nisl maecenas inceptos metus. 
-            Nostra dui iaculis hendrerit nisi nam netus. 
-            </p>
-        <button className="learn-more" onClick={handleLearnMore}> Learn More</button>
+        <p className="paragraph">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Enim morbi nunc
+          turpis hendrerit accumsan, ultricies magna finibus. Eleifend molestie
+          efficitur vulputate porttitor natoque quisque adipiscing. Erat
+          condimentum aenean tempor sagittis sem pharetra pellentesque. Etiam
+          proin metus nisl maecenas inceptos metus. Nostra dui iaculis hendrerit
+          nisi nam netus.
+        </p>
+        <button className="learn-more" onClick={handleLearnMore}>
+          {" "}
+          Learn More
+        </button>
       </div>
     </div>
   );
---
+++ src/components/EventDetailModal/EventDetailModal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./EventDetailModal.css";
+import { useParams } from "react-router-dom";
+
+function EventDetailModal({ events }) {
+  return (
+    <div className="event-modal-container">
+      <div className="event-modal">Event Modal</div>
+    </div>
+  );
+}
+
+export default EventDetailModal;
