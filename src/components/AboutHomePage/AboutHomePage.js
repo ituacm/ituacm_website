214e87a80946a0dd7b77b9aba00210c914c3f60c
İsmet Arca Erdoğar <arcaerdogar@gmail.com>
2024-09-07 18:11:11 +0300

    home page responsive design provided

--- src/components/AboutHomePage/AboutHomePage.js
+++ src/components/AboutHomePage/AboutHomePage.js
@@ -1,54 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './AboutHomePage.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./AboutHomePage.css";
 
 const AboutHomePage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLearnMore = () => {
-        navigate('/about'); 
+  const handleLearnMore = () => {
+    navigate("/about");
   };
-
 
   const [isBetween, setIsBetween] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-        const scrollPosition = window.scrollY;
-        const viewHeight = window.innerHeight;
+      const scrollPosition = window.scrollY;
+      const viewHeight = window.innerHeight;
 
+      let startRange;
 
-        let startRange;
+      if (viewHeight <= 630) {
+        startRange = viewHeight * 2.1;
+      } else if (viewHeight <= 750) {
+        startRange = viewHeight * 1.9;
+      } else {
+        startRange = viewHeight * 1.45;
+      }
 
-        if(viewHeight<=630){
-           startRange = viewHeight * 2.1; 
-        } 
-        else if(viewHeight<=750){
-            startRange = viewHeight * 1.9; 
-        }
-        else{
-            startRange = viewHeight * 1.45; 
-        }
-
-        
-        if (scrollPosition >= startRange) {
-        setIsBetween(true); 
+      if (scrollPosition >= startRange) {
+        setIsBetween(true);
       } else {
         setIsBetween(false);
       }
     };
-        
-        window.addEventListener('scroll', handleScroll);
 
-        
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, []);
-    
-      
-      const combinedClasses = `image-card ${isBetween ? 'scrolled' : ''}`;
-  
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const combinedClasses = `image-card ${isBetween ? "scrolled" : ""}`;
+
   return (
     <div className={combinedClasses}>
       <img
@@ -58,12 +51,18 @@ const AboutHomePage = () => {
       />
       <div className="overlay">
         <h2 className="header">Who are we?</h2>
-        <p className="paragraph">Lorem ipsum odor amet, consectetuer adipiscing elit. Enim morbi nunc turpis hendrerit accumsan, 
-            ultricies magna finibus. Eleifend molestie efficitur vulputate porttitor natoque quisque adipiscing. 
-            Erat condimentum aenean tempor sagittis sem pharetra pellentesque. Etiam proin metus nisl maecenas inceptos metus. 
-            Nostra dui iaculis hendrerit nisi nam netus. 
-            </p>
-        <button className="learn-more" onClick={handleLearnMore}> Learn More</button>
+        <p className="paragraph">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Enim morbi nunc
+          turpis hendrerit accumsan, ultricies magna finibus. Eleifend molestie
+          efficitur vulputate porttitor natoque quisque adipiscing. Erat
+          condimentum aenean tempor sagittis sem pharetra pellentesque. Etiam
+          proin metus nisl maecenas inceptos metus. Nostra dui iaculis hendrerit
+          nisi nam netus.
+        </p>
+        <button className="learn-more" onClick={handleLearnMore}>
+          {" "}
+          Learn More
+        </button>
       </div>
     </div>
   );
