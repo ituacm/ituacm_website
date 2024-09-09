6456523f6af2dbe97db8d275a1a92233075cf46a
İsmet Arca Erdoğar <arcaerdogar@gmail.com>
2024-09-09 23:28:49 +0300

    modal compomnents adjusted.

--- src/components/LectureCard/LectureCard.js
+++ src/components/LectureCard/LectureCard.js
@@ -8,15 +8,21 @@ function LectureCard({ lecture }) {
   return (
     <div className="lecture-card-container">
       <div className="lecture">
-        <h2 className="lecture-number">{"Lecture " + lecture.lectureNumber}</h2>
-        <h2 className="lecture-subject">{lecture.subject}</h2>
-        <div className="lecture-detail-section">
-          <IoCalendarSharp className="course-card-icon" />
-          <p>{lecture.start.toDateString()}</p>
+        <div className="lecture-top">
+          <h2 className="lecture-number">
+            {"Lecture " + lecture.lectureNumber}
+          </h2>
+          <h2 className="lecture-subject">{lecture.subject}</h2>
         </div>
-        <div className="lecture-detail-section">
-          <FaRegClock className="lecture-icon" />
-          <p>{(lecture.end - lecture.start) / (1000 * 60 * 60) + " hours"}</p>
+        <div className="lecture-bottom">
+          <div className="lecture-detail-section">
+            <IoCalendarSharp className="course-card-icon" />
+            <p>{lecture.start.toDateString()}</p>
+          </div>
+          <div className="lecture-detail-section">
+            <FaRegClock className="lecture-icon" />
+            <p>{(lecture.end - lecture.start) / (1000 * 60 * 60) + " hours"}</p>
+          </div>
         </div>
       </div>
     </div>
